e5e26841c2040c9a33e8b8cbc024a266c4b23e8c
Brian Wilhite <bcwilhite@live.com>
2018-09-05 12:24:41 -0400

    created scenario1 run script

---
+++ Templates/1_CreatePlasterManifest/_DemoScenario1.ps1
@@ -0,0 +1,2 @@
+# You can use the Get-PlasterTemplate function to query a list a manifest in a given directory using the -Recurse switch parameter
+Get-PlasterTemplate -Path "$PSScriptRoot\..\" -Recurse
