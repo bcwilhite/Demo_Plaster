0510a626760d037e7def78e0bbd3eafa3749f772
Brian Wilhite <bcwilhite@live.com>
2018-09-06 09:06:37 -0400

    Updated scenario6 demo script

--- Templates/6_NewModuleManifestPrompts/_DemoScenario6.ps1
+++ Templates/6_NewModuleManifestPrompts/_DemoScenario6.ps1
@@ -4,4 +4,5 @@
 
 Invoke-Plaster -TemplatePath $PSScriptRoot -DestinationPath C:\Temp\Demo_Plaster\Scenario6
 
-# Get-ChildItem -Path "$env:LOCALAPPDATA\Plaster" -Filter 'Demo_NewModuleManifestPrompts*' | Get-Content
+# Get-ChildItem -Path "$env:LOCALAPPDATA\Plaster" -Filter 'Demo_NewModuleManifestPrompts*'
+# Get-ChildItem -Path "$env:LOCALAPPDATA\Plaster" -Filter 'Demo_NewModuleManifestPrompts*' | Import-Clixml
